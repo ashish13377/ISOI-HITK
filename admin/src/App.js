7828399aaa1649215b83c2fc120c8c252c44b1bb
ashish13377 <trojan2277@gmail.com>
2021-10-01 17:33:04 +0530

    some update

--- admin/src/App.js
+++ admin/src/App.js
@@ -18,6 +18,7 @@ const App = ({ width }) => {
       ? body.setAttribute("data-sidebar-style", "mini")
       : body.setAttribute("data-sidebar-style", "overlay");
    return (
+      
       <Fragment>
          <Markup />
       </Fragment>
